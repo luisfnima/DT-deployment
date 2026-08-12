47d729fe2b831d6fc15afa35f25d12e556de3de1
xaulltarazona-design <xaulltarazona@gmail.com>
2026-08-12 04:37:52 -0500

    build(backend): push pre-compiled dist Javascript files (report-center/backend/dist/config/theme.js)

---
+++ report-center/backend/dist/config/theme.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.themeConfig = void 0;
+exports.themeConfig = {
+    colors: {
+        primary: '#E53935', // Rojo corporativo
+        background: '#121212', // Gris oscuro
+        surface: '#1E1E1E', // Gris medio
+        border: '#2D2D2D', // Gris claro
+        text: '#FFFFFF', // Blanco
+        textMuted: '#9CA3AF' // Gris de texto secundario
+    }
+};
