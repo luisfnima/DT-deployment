6b872eeced51824fb148046dc0f906666a8ccfed
xaulltarazona-design <xaulltarazona@gmail.com>
2026-08-12 04:37:50 -0500

    build(backend): push pre-compiled dist Javascript files (report-center/backend/dist/config/scheduler.js)

---
+++ report-center/backend/dist/config/scheduler.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.schedulerConfig = void 0;
+exports.schedulerConfig = {
+    checkIntervalMs: 10000, // Check scheduled reports every 10 seconds (for MVP simulation)
+    maxConcurrentJobs: 3,
+    retryAttempts: 2
+};
