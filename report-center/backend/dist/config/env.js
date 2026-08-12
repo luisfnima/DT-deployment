60aedcd4ad38630c3b27af262281db87a0e70544
xaulltarazona-design <xaulltarazona@gmail.com>
2026-08-12 04:37:48 -0500

    build(backend): push pre-compiled dist Javascript files (report-center/backend/dist/config/env.js)

---
+++ report-center/backend/dist/config/env.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.env = void 0;
+const dotenv_1 = __importDefault(require("dotenv"));
+const path_1 = __importDefault(require("path"));
+// Load .env from backend root directory
+dotenv_1.default.config({ path: path_1.default.join(__dirname, '../../.env') });
+exports.env = {
+    PORT: process.env.PORT || 5000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    EVOLUTION_API_KEY: process.env.EVOLUTION_API_KEY || 'NEXO_SECRET_KEY_2026',
+    EVOLUTION_API_URL: process.env.EVOLUTION_API_URL || 'http://localhost:8080',
+    CRM_API_KEY: process.env.CRM_API_KEY || 'crm_secret_key',
+    CRM_API_URL: process.env.CRM_API_URL || 'https://api.crm.dreamteam.com',
+    WHATSAPP_RECIPIENT: process.env.WHATSAPP_RECIPIENT || '+51 987 654 321'
+};
