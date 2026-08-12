9151402c6ef2260e8342a23fc4dc1b6d51ac202c
xaulltarazona-design <xaulltarazona@gmail.com>
2026-08-12 04:38:19 -0500

    build(backend): push pre-compiled dist Javascript files (report-center/backend/dist/services/channels/DiscordChannel.js)

---
+++ report-center/backend/dist/services/channels/DiscordChannel.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.DiscordChannel = void 0;
+class DiscordChannel {
+    async sendMessage(recipient, content) {
+        console.log(`[Discord Channel] Enviando reporte a Webhook/Usuario ${recipient}...`);
+        // Stub implementation
+        return true;
+    }
+}
+exports.DiscordChannel = DiscordChannel;
